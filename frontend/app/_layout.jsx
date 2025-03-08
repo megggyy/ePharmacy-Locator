d2a52c97dc8187f2de1beab3eb1287b45e23db9d
megggyy <shanaimeg.honrado@tup.edu.ph>
2025-03-09 04:13:55 +0800

    LAYOUT NAIWAN

--- frontend/app/_layout.jsx
+++ frontend/app/_layout.jsx
@@ -71,6 +71,7 @@ export default function RootLayout() {
             <Stack.Screen name="screens/PharmacyOwner/Account/CheckLicense" options={{ headerShown: false }} />
             <Stack.Screen name="screens/PharmacyOwner/Account/PharmacyOwnerSignupScreen" options={{ headerShown: false }} />
             <Stack.Screen name="screens/PharmacyOwner/Dashboard" options={{ headerShown: false }} />
+            <Stack.Screen name="screens/PharmacyOwner/Charts/ExpiringStocksChart" options={{ headerShown: false }} />
 
             {/* PROFILE */}
             <Stack.Screen name="screens/PharmacyOwner/Profile/ViewProfile" options={{ headerShown: false }} />
