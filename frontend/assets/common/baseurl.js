639b60a1d6adf72c2ea234835ac210e2c0fa6556
megggyy <shanaimeg.honrado@tup.edu.ph>
2025-06-12 21:49:52 +0800

    BUILD

--- frontend/assets/common/baseurl.js
+++ frontend/assets/common/baseurl.js
@@ -10,12 +10,6 @@ if (Platform.OS === 'ios' || Platform.OS === 'android') {
     baseURL = 'https://epharmacylocator-backend.onrender.com/api/v1/';
 }
 
-// if (Platform.OS === 'ios' || Platform.OS === 'android') {
-//     baseURL = 'http://192.168.68.111:4000/api/v1/';
-// } else {
-//     baseURL = 'http://192.168.68.200:4000/api/v1/';
-// }
-
 export default baseURL;
 
 //   baseURL = 'https://epharmacylocator-backend.onrender.com/api/v1/';
--- frontend/eas-build-pre-install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Running npm install with --legacy-peer-deps"
-npm install --legacy-peer-deps
